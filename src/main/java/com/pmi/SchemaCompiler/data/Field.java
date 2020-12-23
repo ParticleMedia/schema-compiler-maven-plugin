5a55406f7126a4302ab87e50d5e0ecf1cced7199
Peiling Ding <peiling.ding@newsbreak.com>
2020-12-22 20:09:03 -0800

    Handle list of string

--- src/main/java/com/pmi/SchemaCompiler/data/Field.java
+++ src/main/java/com/pmi/SchemaCompiler/data/Field.java
@@ -20,6 +20,7 @@ public class Field {
   private String setterName;
   private boolean isList;
   private String clonerName;
+  private boolean useClone;
 
   public void setType(String type) {
     this.type = TypeUtil.parseGenericType(type);
@@ -36,6 +37,28 @@ public class Field {
       this.clonerName = this.getterName;
     } else {
       this.clonerName = this.name + ".clone";
+    }
+
+    switch (this.type) {
+      case "String":
+      case "Integer":
+      case "Boolean":
+      case "Double":
+      case "Float":
+      case "int":
+      case "boolean":
+      case "double":
+      case "float":
+      case "List<String>":
+      case "List<Integer>":
+      case "List<Boolean>":
+      case "List<Double>":
+      case "List<Float>":
+        this.useClone = false;
+        break;
+      default:
+        this.useClone = true;
+        break;
     }
   }
 }
